aa694ae8857f6e96f4695358baf90a7fab3e7a33
augie <schaua@augustschau.com>
2016-08-29 10:39:50 +0530

    RawData

--- RestClient/MainWindow.xaml.cs
+++ RestClient/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,19 @@ namespace RestClient
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            WebClient wc = new WebClient();
+            wc.BaseAddress = "http://localhost:62349/api/";
+            var result = wc.DownloadString("Values");
+            this.DataContext = result;
+        }
+
+        private void button_Serialize_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
